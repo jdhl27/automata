f9e3910653edaefe76ba2d753575fe814d6e86ed
jdhl27 <independientemedellin.27.03@gmail.com>
2023-11-06 23:31:49 -0500

    Add color for graph

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+// Colores para los nodos de los graficos
+const colorNode = {
+  border: "#000000",
+  background: "#1e9254",
+  hover: {
+    border: "#000000",
+    background: "#9cd7ff",
+  },
+  highlight: {
+    border: "#000000",
+    background: "#1e9254",
+  },
+};
+
+const colorNodeReject = {
+  border: "#000000",
+  background: "#00c3f8",
+  hover: {
+    border: "#000000",
+    background: "#9cd7ff",
+  },
+  highlight: {
+    border: "#000000",
+    background: "#00c3f8",
+  },
+};
 
 function App() {
   return (
