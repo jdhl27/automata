8e6634e3332a785e3adbfaade920ab5419a3bd4d
jdhl27 <independientemedellin.27.03@gmail.com>
2023-11-06 23:28:23 -0500

    Create component input

---
+++ src/components/input/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./styles.css";
+
+function Input({
+  type = "text",
+  placeholder = "",
+  autofocus = false,
+  autocomplete = "true",
+  required = true,
+  label = "",
+  onchange = function () {},
+  disabled = false,
+}) {
+  return (
+    <div className="container-field">
+      <label className="label">{label}</label>
+      <input
+        type={type}
+        placeholder={placeholder}
+        className="input"
+        autoFocus={autofocus}
+        autoComplete={autocomplete}
+        required={required}
+        onChange={(e) => onchange(e.target.value)}
+        disabled={disabled}
+      />
+    </div>
+  );
+}
+
+export default Input;
